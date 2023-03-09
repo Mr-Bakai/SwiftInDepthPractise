5a6402280046480a86813a820f8471ad6c812ae6
Mr-Bakai <ismailov.time@gmail.com>
2023-03-09 20:06:08 +0600

    Initial Commit

---
+++ SwiftInDepth/SwiftInDepthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftInDepthApp.swift
+//  SwiftInDepth
+//
+//  Created by Bakai on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftInDepthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
