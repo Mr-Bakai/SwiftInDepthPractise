6b29d2f70f37362ac7349a792e139479f4296c2b
Bakai Ismaiilov <ismailov.time@gmail.com>
2024-06-23 15:00:36 +0600

    MapsPlayground is added

--- SwiftInDepth/Practise/MapsPlayground.playground/Contents.swift
+++ SwiftInDepth/Practise/MapsPlayground.playground/Contents.swift
@@ -1,11 +1,3 @@
-//
-//  Maps.swift
-//  SwiftInDepth
-//
-//  Created by Bakai Ismaiilov on 23/6/24.
-//
-
-import Foundation
 
 let commitStats = [
     (name: "Miranda", count: 30),
@@ -13,10 +5,8 @@ let commitStats = [
     (name: "John", count: 0)
 ]
 
-func mapFoo() {
-    let readableStats = resolveCounts(statistics: commitStats)
-    print(readableStats) // ["Miranda isn't very active on the project", "Elly is quite active", "John isn't involved in the project"]
-}
+let readableStats = resolveCounts(statistics: commitStats)
+print(readableStats) // ["Miranda isn't very active on the project", "Elly is quite active", "John isn't involved in the project"]
 
 func resolveCounts(statistics: [(String, Int)]) -> [String] {
     var resolvedCommits = [String]()
@@ -37,11 +27,11 @@ func resolveCounts(statistics: [(String, Int)]) -> [String] {
 // same one but with a map
 func resolveCountsMap(statistics: [(String, Int)]) -> [String] {
     return statistics.map { (name: String, count: Int) -> String in
-         switch count {
-         case 0: return "\(name) isn't involved in the project."
-         case 1..<100: return "\(name) isn't very active on the project."
-         default: return "\(name) is active on the project."
-         }
+        switch count {
+        case 0: return "\(name) isn't involved in the project."
+        case 1..<100: return "\(name) isn't very active on the project."
+        default: return "\(name) is active on the project."
+        }
     }
 }
 
@@ -61,3 +51,22 @@ func countsMap(statistics: [(String, Int)]) -> [Int] {
         .filter { $0 > 0 }
         .sorted(by: >)
 }
+
+
+print(commitStats) // [(name: "Miranda", count: 30), (name: "Elly", count: 650), (name: "John", count: 0)]
+let commitsDict = Dictionary(uniqueKeysWithValues: commitStats)
+print(commitsDict) // ["Miranda": 30, "Elly": 650, "John": 0]
+print(commitsDict) // ["Miranda": 30, "Elly": 650, "John": 0]
+
+let mappedKeysAndValues = commitsDict.map { (name: String, count: Int) ->
+    String in
+    switch count {
+    case 0: return "\(name) isn't involved in the project."
+    case 1..<100: return "\(name) isn't very active on the project."
+    default: return "\(name) is active on the project."
+    }
+}
+
+print(mappedKeysAndValues) // ["Miranda isn't very active on the project", "Elly is active on the project", "John isn't involved in the project"]
+
+
